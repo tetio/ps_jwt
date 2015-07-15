cab8773cec4bce2135ec343f46002f4ad0630396
Sergi Maymi <smaymi.work@gmail.com>
2015-07-15 06:41:59 +0200

    fixed css link

--- api/api.js
+++ api/api.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
+app.use(bodyParser.json());
+
 app.post('/register', function(req, res){
-	res.send('Hi!');
+	res.send('Hi!')
 });
 
 var server = app.listen(3000, function() {
